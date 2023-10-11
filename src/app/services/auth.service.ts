e53004e5e0c08592044409086a76802110df5d9a
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-10-11 13:20:18 +0200

    header component - done

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   login(username:string, password:string)
   {
@@ -16,5 +17,9 @@ export class AuthService {
     {
       return 403
     }
+  }
+  logout()
+  {
+    this.router.navigate(['login'])
   }
 }
